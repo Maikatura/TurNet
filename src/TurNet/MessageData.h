5dae3327c7312d4bb8122bf38c57685f7ebeb97f
Zyro / Maikatura <31046190+Maikatura@users.noreply.github.com>
2022-03-31 14:16:02 +0200

    Poggers

--- src/TurNet/MessageData.h
+++ src/TurNet/MessageData.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <vector>
+
+// store plain old data in a vector.
+class MessageData
+{
+public:
+    void operator<<(const std::vector<char>& data);
+    void operator>>(std::vector<char>& data);
+private:
+    std::vector<int> myData;
+};
